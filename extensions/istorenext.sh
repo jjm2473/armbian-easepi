183b89ec5ce94e4be9ad6700cbffec699887c6df
Liangbin Lian <jjm2473@gmail.com>
2026-03-02 18:11:54 +0800

    istorenext: disable dhcpcd on all interfaces
    Signed-off-by: Liangbin Lian <jjm2473@gmail.com>

--- extensions/istorenext.sh
+++ extensions/istorenext.sh
@@ -97,6 +97,7 @@ post_post_debootstrap_tweaks__istorenext_network() {
 
 	chroot_sdcard systemctl --no-reload enable networking.service
 	chroot_sdcard systemctl --no-reload enable istorenext-init-network.service
+	chroot_sdcard systemctl --no-reload disable dhcpcd.service
 
 	# change resolv.conf after all packages installed, since this will break network in chroot,
 	# but we want to use openresolv instead of systemd-resolved in the final image.
