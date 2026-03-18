568795c9fd7d5aeb6eecfc84610a33503e51f4b8
Liangbin Lian <jjm2473@gmail.com>
2026-03-18 17:57:31 +0800

    extensions/sing-box: downgrade to version 1.10.7
    for config file compatibility.
    
    Signed-off-by: Liangbin Lian <jjm2473@gmail.com>

--- extensions/sing-box.sh
+++ extensions/sing-box.sh
@@ -5,16 +5,16 @@ function pre_install_distribution_specific__sing-box() {
 	local CACHEDIR="${SRC}/cache/sing-box"
 	echo "Try download sing-box"
 	mkdir -p "${CACHEDIR}"
-	local version="1.13.3"
+	local version="1.10.7"
 	if [[ "${ARCH}" = "amd64" ]]; then
-		sha256='8f5336cc671851147b695b28bb69a8ae9e7b7bb9ad0513a2099a1e9be413be8f'
+		sha256='1951a0785c8b4e1e21e0640227a49528ca772aec3d680061652e3d6b687e00fe'
 	elif [[ "${ARCH}" = "arm64" ]]; then
-		sha256='7c0bf8918f1c558d184b2a8c10855bbe3e14f813322b3354e42104937c8c9870'
+		sha256='15b43a0a50b4e6962aca819d4f3055aaac75ca7481350d4aaebe93ed06b7af49'
 	else
 		echo "sing-box is unsupported in this ARCH: ${ARCH}"
 		return 1
 	fi
-	local filename="sing-box-${version}-linux-${ARCH}-glibc.tar.gz"
+	local filename="sing-box-${version}-linux-${ARCH}.tar.gz"
 
 	[[ -e "${CACHEDIR}/${filename}" ]] && \
 		echo "${sha256} ${CACHEDIR}/${filename}" | sha256sum -c - && {
@@ -44,8 +44,8 @@ function pre_install_distribution_specific__sing-box() {
 
 function pre_customize_image__install_sing-box() {
 	display_alert "Install sing-box..." "${EXTENSION}" "info"
-	local version="1.13.3"
-	local dirname="sing-box-${version}-linux-${ARCH}-glibc"
+	local version="1.10.7"
+	local dirname="sing-box-${version}-linux-${ARCH}"
 	local path="${SRC}/cache/sing-box/${dirname}.tar.gz"
 	mkdir -p "${SDCARD}/usr/local/bin"
 	tar -xzf "${path}" -C "${SDCARD}/usr/local/bin" --strip-components=1 "${dirname}/sing-box"
